6bd6267c9db5ffd71b287327ed5d61cc2d3a3cb0
Gabriel <gabrieloliveira280807@gmail.com>
2026-05-27 16:16:45 -0300

    feat: Implementando TestaArrayList, para funcionar corretamente.

--- ListContinuidade/src/TestaArrayList.java
+++ ListContinuidade/src/TestaArrayList.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class TestaArrayList {
     public static void main(String[] args) {
         List<Contato> listaTelefonica = new ArrayList<>();
+        Contato item = new Contato("joao", "15 91234-9292");
+
+        listaTelefonica.add(item);
+
+        System.out.println("A lista tem " + listaTelefonica.size() + " itens");
+
+        listaTelefonica.remove(0);
+
+        System.out.println("A lista tem " + listaTelefonica.size() + " itens");
     }
 
 
