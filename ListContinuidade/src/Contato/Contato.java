132147205a1129ed11ca66fe53109f58775e1451
Gabriel <gabrieloliveira280807@gmail.com>
2026-05-27 16:08:05 -0300

    commit initial, adicionando arquivos de referencias para estudo com base na lista de exercícios de estrutura de dados

---
+++ ListContinuidade/src/Contato/Contato.java
@@ -0,0 +1,32 @@
+package Contato;
+
+public class Contato {
+    private String nome;
+    private String telefone;
+
+    public Contato(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public boolean equals(Object o) {
+        Contato outro = (Contato) o;
+        return  this.nome.equals(outro.nome);
+    }
+}
---
+++ ListContinuidade/src/TestaArrayList.java
@@ -0,0 +1,12 @@
+import Contato.Contato;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestaArrayList {
+    public static void main(String[] args) {
+        List<Contato> listaTelefonica = new ArrayList<>();
+    }
+
+
+}
